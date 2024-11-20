8cede68d0952aa5cdf322ac601c01ed62dbdf50e
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-11-20 14:19:17 -0500

    added native wind for tailwind css support, added daisyui plugin migrated website themes

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex flex-col items-center justify-center h-screen bg-red-900"
+    >
+      <Text className="bg-primary btn-disabled">TESTESTEST</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui'),  ],
+  daisyui: {
+    
+    themes:[ 
+      {
+        mytheme: { // Light theme
+          "primary": "#2e046d", // Purple
+          "secondary": "#e7ac3b", // Yellow
+          "accent": "#6c703e", // Green
+          "neutral": "#444444", // Gray
+          "base-100": "#ffffff", // White
+          "info": "#C8E1E7",
+          "success": "#DEF29F",
+          "warning": "#F7E589",
+          "error": "#F2B6B5",
+        }, 
+        IIA: { 
+          "primary": "#3EB169", // Green
+          "secondary": "#F3FAF6", // light green
+          "accent": "#6c703e", // Green
+          "neutral": "#444444", // Gray
+          "base-100": "#ffffff", // White
+          "info": "#C8E1E7",
+          "success": "#DEF29F",
+          "warning": "#F7E589",
+          "error": "#F2B6B5",
+        }, 
+        
+      },
+    ],
+    darkTheme:"mytheme",
+  },
+}
