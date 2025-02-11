695824a938b32dc772a35935302c1559ccc4f59d
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2025-02-11 15:11:32 -0500

    page completed, fix alignment issue and fetch from database, make timings into a componenet

--- app/prayertimes/index.tsx
+++ app/prayertimes/index.tsx
@@ -1,5 +1,5 @@
 import PrayerCard from "@/app/componenets/PrayerCard"
-import { ChevronLeft, ChevronRight, Calendar } from "lucide-react-native"
+import { ChevronLeft, ChevronRight, Calendar, Sunrise } from "lucide-react-native"
 import { View, Text, Pressable, Alert } from "react-native"
 export default function index() {
     return (
@@ -30,11 +30,63 @@ export default function index() {
             </View>
             <View className="flex flex-row justify-between w-full p-2 ">
                 <Text className="px-8 text-lg font-bold">Prayer</Text>
-                <View className="flex flex-row space-x-16">
+                <View className="flex flex-row space-x-24 font-medium">
                     <Text className="text-lg ">Athan</Text>
                     <Text className="text-lg ">Iqama</Text>
                 </View>
 
+            </View>
+            <View className="space-y-2">
+                <View className="flex flex-row justify-between w-full p-2 rounded-md bg-slate-50 ">
+                    <View className="flex flex-row space-x-2">
+                        <Sunrise className=" color-purple-600" />
+                        <Text className="text-lg text-bold ">Fajr</Text>
+                    </View>
+                    <View className="flex flex-row space-x-8 font-semibold">
+                        <Text className="text-lg text-gray-600">6:11 am</Text>
+                        <Text className="text-lg">6:15 am</Text>
+                    </View>
+                </View>
+                <View className="flex flex-row justify-between w-full p-2 py-4 rounded-md bg-slate-50 ">
+                    <View className="flex flex-row space-x-2">
+                        <Sunrise className=" color-purple-600" />
+                        <Text className="text-lg text-bold ">Fajr</Text>
+                    </View>
+                    <View className="flex flex-row space-x-8 font-semibold">
+                        <Text className="text-lg text-gray-600">6:11 am</Text>
+                        <Text className="text-lg">6:15 am</Text>
+                    </View>
+                </View>
+                <View className="flex flex-row justify-between w-full p-2 py-4 rounded-md bg-slate-50 ">
+                    <View className="flex flex-row space-x-2">
+                        <Sunrise className=" color-purple-600" />
+                        <Text className="text-lg text-bold ">Fajr</Text>
+                    </View>
+                    <View className="flex flex-row space-x-8 font-semibold">
+                        <Text className="text-lg text-gray-600">6:11 am</Text>
+                        <Text className="text-lg">6:15 am</Text>
+                    </View>
+                </View>
+                <View className="flex flex-row justify-between w-full p-2 py-4 rounded-md bg-slate-50 ">
+                    <View className="flex flex-row space-x-2">
+                        <Sunrise className=" color-purple-600" />
+                        <Text className="text-lg text-bold ">Fajr</Text>
+                    </View>
+                    <View className="flex flex-row space-x-8 font-semibold">
+                        <Text className="text-lg text-gray-600">6:11 am</Text>
+                        <Text className="text-lg">6:15 am</Text>
+                    </View>
+                </View>
+                <View className="flex flex-row justify-between w-full p-2 py-4 rounded-md bg-slate-50 ">
+                    <View className="flex flex-row space-x-2">
+                        <Sunrise className=" color-purple-600" />
+                        <Text className="text-lg text-bold ">Fajr</Text>
+                    </View>
+                    <View className="flex flex-row space-x-8 font-semibold">
+                        <Text className="text-lg text-gray-600">6:11 am</Text>
+                        <Text className="text-lg">6:15 am</Text>
+                    </View>
+                </View>
             </View>
         </View>
     )
