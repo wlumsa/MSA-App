5ead66b8620c16153078b3de814308d9459d78c2
rzlm <razan4424@gmail.com>
2025-05-19 14:01:00 -0400

    add app.config.ts to manage environments

---
+++ app.config.ts
@@ -0,0 +1,121 @@
+import { ConfigContext, ExpoConfig } from "expo/config";
+import { version } from "./package.json";
+
+// Replace these with your EAS project ID and project slug.
+// You can find them at https://expo.dev/accounts/[account]/projects/[project].
+const EAS_PROJECT_ID = "bb19fcf6-e5e9-40bd-8035-da978b38be8d";
+const PROJECT_SLUG = "msa-app";
+const OWNER = "msateam";
+
+// App production config
+const APP_NAME = "MSA App";
+const BUNDLE_IDENTIFIER = "com.msa.msa-app";
+const PACKAGE_NAME = "com.msa.msa-app";
+const ICON = "./assets/images/MSA_Logo.png";
+const ADAPTIVE_ICON = "./assets/images/MSA_Logo.png";
+const SCHEME = "app-scheme";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  console.log("⚙️ Building app for environment:", process.env.APP_ENV);
+  const { name, bundleIdentifier, icon, adaptiveIcon, packageName, scheme } =
+    getDynamicAppConfig(
+      (process.env.APP_ENV as "development" | "preview" | "production") ||
+        "development"
+    );
+
+  return {
+    ...config,
+    name: name,
+    version, // Automatically bump your project version with `npm version patch`, `npm version minor` or `npm version major`.
+    slug: PROJECT_SLUG, // Must be consistent across all environments.
+    orientation: "portrait",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    icon: icon,
+    scheme: scheme,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: bundleIdentifier,
+      infoPlist: {
+    ITSAppUsesNonExemptEncryption: false
+  }
+      
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: adaptiveIcon,
+        backgroundColor: "#ffffff",
+      },
+      package: packageName,
+    },
+    updates: {
+      url: `https://u.expo.dev/${EAS_PROJECT_ID}`,
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    extra: {
+      eas: {
+        projectId: EAS_PROJECT_ID,
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    owner: OWNER,
+  };
+};
+
+// Dynamically configure the app based on the environment.
+// Update these placeholders with your actual values.
+export const getDynamicAppConfig = (
+  environment: "development" | "preview" | "production"
+) => {
+  if (environment === "production") {
+    return {
+      name: APP_NAME,
+      bundleIdentifier: BUNDLE_IDENTIFIER,
+      packageName: PACKAGE_NAME,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: SCHEME,
+    };
+  }
+
+  if (environment === "preview") {
+    return {
+      name: `${APP_NAME} Preview`,
+      bundleIdentifier: `${BUNDLE_IDENTIFIER}.preview`,
+      packageName: `${PACKAGE_NAME}.preview`,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: `${SCHEME}-prev`,
+    };
+  }
+
+  return {
+    name: `${APP_NAME} Development`,
+    bundleIdentifier: `${BUNDLE_IDENTIFIER}.dev`,
+    packageName: `${PACKAGE_NAME}.dev`,
+    icon: ICON,
+    adaptiveIcon: ADAPTIVE_ICON,
+    scheme: `${SCHEME}-dev`,
+  };
+};
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,7 +37,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="halalfood"
+        name="Halalfood"
         options={{
           title: 'Halal Food',
           tabBarIcon: ({ color }) => <MapPin size={24}  color={color} strokeWidth={2.5} />,
