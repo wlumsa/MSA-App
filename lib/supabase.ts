cc0ace3d8e7c5f1a3b236057771aa4c9ee9d0378
rzlm <razan4424@gmail.com>
2025-06-13 21:00:07 -0400

    renamed supabase variable

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
     process.env.EXPO_PUBLIC_SUPABASE_URL || '',
-    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '',
+    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || ''
 )
